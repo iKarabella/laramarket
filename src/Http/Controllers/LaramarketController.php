d2f6d13225a866d19f95bd160356826f6411e376
Konstantin Gordeev <zlaya@younan.ru>
2025-04-29 12:11:03 +0000

    4

---
+++ src/Http/Controllers/LaramarketController.php
@@ -0,0 +1,14 @@
+<?php
+namespace Karabella\Laramarket\Http\Conrollers;
+
+
+class LaramarketController extends Controller
+{
+    /**
+     * Страница управления турниром
+     */
+    public function index()
+    {
+        return "Laramarket Conroller";
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,4 +1,7 @@
 <?php
-namespace Karabella\Laramarket\routes
+namespace Karabella\Laramarket\routes;
+
+use Karabella\Laramarket\Http\Controllers\LaramarketController;
 
 Route::get('/laramarket', function(){return "Laramarket route test";});
+Route::get('/laramarket2', [LaramarketController::class, 'index']);
